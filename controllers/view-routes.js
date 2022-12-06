aa21912b8560cac6b918568977e73f0b27fb0b44
Cody <110630101+CodySaal@users.noreply.github.com>
2022-12-05 18:17:56 -0600

    Home screen blog posts now show. Need formatting though

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const { Blog, Comment, User } = require("../models");
 
-router.get("/", (req, res) => {
-    console.log(req)
-    res.render("home")
+router.get("/", async (req, res) => {
+    try {
+        let blogs = await Blog.findAll({
+            include: User
+        })
+        blogs = blogs.map(blog => blog.get({ plain: true }))
+        res.render("home", {
+            blogs
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
 })
 
 
