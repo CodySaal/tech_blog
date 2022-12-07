9782016e879777cb8d87281385686b52ba1dbd53
Cody <110630101+CodySaal@users.noreply.github.com>
2022-12-07 09:57:58 -0600

    started on dashboard routes and views

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -16,6 +16,43 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+        let blogs = await Blog.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            include: User
+        })
+        blogs = blogs.map(blog => blog.get({ plain: true }))
+        res.render("dashboard", {
+            blogs,
+            url: req.originalUrl,
+            logged_in: req.session.logged_in
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get("/createblog", withAuth, async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                id: req.session.user_id
+            }
+        })
+        user = user.get({ plain: true })
+        res.render("createblog", {
+            user,
+            url: req.originalUrl,
+            logged_in: req.session.logged_in
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 router.get("/login", (req, res) => {
     res.render("login")
     
